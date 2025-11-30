b0b7a0cade514377a8e55e4d17d78da95f0cc784
girvel <widauka@ya.ru>
2025-11-30 21:26:28 +0500

    Set up stack

--- boot.asm
+++ boot.asm
@@ -2,12 +2,35 @@ bits 16  ; 16-bit mode
 org 0x7c00  ; bootloader address
 
 start:
-    mov ah, 0x0e
-    mov al, 'X'
-    int 0x10
+    ; --- STACK SETUP ---
+    cli
+    xor ax, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    ; --- DATA SEGMENT SETUP ---
+    mov es, ax
+    mov ds, ax
+
+    mov al, 'H'
+    call print_char
+
+    mov al, 'i'
+    call print_char
 
 hang:
     jmp hang
+
+; --- FUNCTIONS ---
+
+print_char:
+    ; AL receives the character
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+; --- PADDING ---
 
 times 510-($-$$) db 0
 dw 0xaa55
