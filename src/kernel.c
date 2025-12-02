40b01ea0e693f954e3bc84c0646c1d1672992aa2
girvel <widauka@ya.ru>
2025-12-02 20:07:29 +0500

    Do argc/argv-like thing

--- src/kernel.c
+++ src/kernel.c
@@ -36,16 +36,18 @@ void run() {
         tty_write(literal("\n> "));
         tty_read(&cmd_buffer, .end = '\n');
 
-        String cmd = slice(String, cmd_buffer);
-        if (string_starts_with(cmd, literal("echo"))) {
-            tty_write(string_sub(cmd, 5, cmd.length));
-        } else if (string_starts_with(cmd, literal("split"))) {
-            StringArray words = string_split(cmd, &heap, ' ');
-            enumerate (address, i, String *, word, &words) {
+        String cmd_full = slice(String, cmd_buffer);
+        StringArray args = string_split(cmd_full, &heap, ' ');
+        String cmd = args.base[0];
+
+        if (string_equal(cmd, literal("echo"))) {
+            tty_write(string_sub(cmd_full, 5, cmd_full.length));
+        } else if (string_equal(cmd, literal("split"))) {
+            enumerate (address, i, String *, word, &args) {
                 if (i > 0) tty_write(literal("\n"));
                 tty_write(*word);
             }
-        } else if (string_starts_with(cmd, literal("clear"))) {
+        } else if (string_equal(cmd, literal("clear"))) {
             tty_clear();
         } else {
             tty_write(literal("Unknown command"));
