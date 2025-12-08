9c1ddf0bd5b1ace0704167f48b6f3e3f66047d8a
girvel <widauka@ya.ru>
2025-12-09 01:07:04 +0500

    Sketch placement new

--- src/modern/allocator.hpp
+++ src/modern/allocator.hpp
@@ -21,3 +21,17 @@ struct allocator {
     virtual void *allocate_raw(address bytes) = 0;
     virtual void deallocate_raw(void *base) = 0;
 };
+
+using legacy_address = decltype(sizeof(0));
+
+// Needed by C++
+inline void *operator new(legacy_address, void *ptr) { return ptr; }
+inline void operator delete(void *, void*) {}
+
+inline void *operator new(legacy_address size, allocator &alloc) {
+    return alloc.allocate_raw(size);
+}
+
+inline void operator delete(void *ptr, allocator &alloc) {
+    return alloc.deallocate_raw(ptr);
+}
