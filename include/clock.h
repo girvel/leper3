0dc5135d04b6a7efb9cb7501fc87568e12ae2510
girvel <widauka@ya.ru>
2026-03-21 13:46:22 +0500

    Write docs for existing headers

--- include/clock.h
+++ include/clock.h
@@ -10,8 +10,15 @@ typedef struct {
     u8 year;
 } clock_Time;
 
+// Read the current clock state
 clock_Time clock_read();
+
+// Get the current clock frame
 u64 clock_frame();
+
+// Get an approximation of the current millisecond
 u32 clock_ms();
+
+// Conversion rate between frames & seconds
 extern u32 clock_MHz;
 
--- include/kb.h
+++ include/kb.h
@@ -8,5 +8,6 @@ typedef enum {
     kb_Key_right = 131,
 } kb_Key;
 
-// Reads current keyboard state;
+// Reads current keyboard state; returns 0 if nothing's pressed, else a valid character or a kb_Key;
+// any kb_Key is >= 128.
 u8 kb_read();
